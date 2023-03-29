21d012df70337e879482339bbf650ee12cb0ce73
Blaz Zorec <blaz.zorec@endava.com>
2023-03-29 13:47:59 +0200

    file read input numbers

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -12,13 +12,15 @@ namespace ConsoleApplication
                 Console.WriteLine("Usage: dn1 <input_file>");
                 return;
             }
-            
+
             string inputFilePath = args[0];
             if (!File.Exists(inputFilePath))
             {
                 Console.WriteLine("Input file not found.");
                 return;
             }
+
+            string[] inputNumbers = File.ReadAllText(inputFilePath).Split(' ');
         }
     }
 }
