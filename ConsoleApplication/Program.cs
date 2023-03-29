4191fdd37ce20362b5c66e7ded8f6e658d7ea1c0
Blaz Zorec <blaz.zorec@endava.com>
2023-03-29 13:45:29 +0200

    added args check

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ConsoleApplication
+﻿using System;
+
+namespace ConsoleApplication
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: dn1 <input_file>");
+                return;
+            }
         }
     }
 }
